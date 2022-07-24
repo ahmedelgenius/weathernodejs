fb29780a2d19a4c3d3219055d36999ba53dd0a14
ahmedhany <elgenius10000@gmail.com>
2022-07-24 19:56:31 +0200

    weather project

---
+++ src/utils/geocode.js
@@ -0,0 +1,24 @@
+const request = require("request");
+
+const geocode = (address, callback) => {
+  const url =
+    "https://api.mapbox.com/geocoding/v5/mapbox.places/" +
+    address +
+    ".json?access_token=pk.eyJ1IjoiZWxnZW5pdXMiLCJhIjoiY2w1ejlhcG1qMGxseTNicDJ0cTRhdTlldSJ9.uJpVChQ4h2ngrVnHro3ypQ&limit=1";
+
+  request({ url, json: true }, (error, { body }) => {
+    if (error) {
+      callback("Unable to connect to location services", undefined);
+    } else if (body.features.length === 0) {
+      callback("Unable to find location. Try another search.", undefined);
+    } else {
+      callback(undefined, {
+        latitude: body.features[0].center[1],
+        longitude: body.features[0].center[0],
+        location: body.features[0].place_name,
+      });
+    }
+  });
+};
+
+module.exports = geocode;
